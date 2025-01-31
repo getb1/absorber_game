d5cb316879103ab0d408d3f6dd09770edc9d6a7d
getb1 <78261811+getb1@users.noreply.github.com>
2025-01-31 15:59:14 +0000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,38 +1,78 @@
 from pygame_functions import *
 import math, random, time, pickle
 
-screenSize(1000,1000)
-setBackgroundColour("black")
 
-SCRREN_SIZE = 100
+
+SCREEN_SIZE_W = 1800
+SCREEN_SIZE_H = 1000
+
+screenSize(SCREEN_SIZE_W,SCREEN_SIZE_H)
+setBackgroundColour("black")
 
 class Character:
 
     def __init__(self,x,y,image,size):
 
-        self._x = x
-        self._y = y
-        self._size = size
-        self._sprite = makeSprite(image)
-        moveSprite(self._sprite,self._x,self._y,centre=True)
-        transformSprite(self._sprite, 0, self._size/100)
-        showSprite(self._sprite)
+        self.x = x
+        self.y = y
+        self.size = size
+        self.sprite = makeSprite(image)
+        moveSprite(self.sprite,self.x,self.y,centre=True)
+        transformSprite(self.sprite, 0, self.size/100)
+        showSprite(self.sprite)
+
 class Creature(Character):
     def __init__(self,x,y,image,  size):
         super().__init__(x,y,image,size)
         
-        self.x_speed = 0
-        self.y_speed = 0
-
+        self.speed = random.uniform(1,0)
+        self.angle_deg = random.randint(0,360)
         
+        
+        transformSprite(self.sprite, self.angle_deg,self.size/100)
+
+    
+    def move(self):
+        
+        self.x = ((math.cos(math.radians(self.angle_deg))*self.speed)+self.x)%SCREEN_SIZE_W
+        self.y = ((math.sin(math.radians(self.angle_deg))*self.speed)+self.y)%SCREEN_SIZE_H
+
+        moveSprite(self.sprite,self.x,self.y,centre=True)
+
+class Player(Creature):
+
+    def __init__(self,x,y,image,  size):
+        super().__init__(x,y,image, size)
+    
+    def move(self):
+        mouse_x = mouseX()
+        mouse_y = mouseY()
+
+        diff_x = mouse_x-self.x
+        diff_y = mouse_y-self.y
+
+        self.angle_deg = math.degrees(math.atan(diff_y/diff_x))
+
+
+        transformSprite(self.sprite,self.angle_deg,self.size)
 
 
 
 
 setAutoUpdate(False)
-for i in range(40):
-    c1 = Creature(random.randint(0,1000),random.randint(0,1000), "Image_Files/sprite_0.png", random.randint(100,400)) 
+enemies = []
+for i in range(30):
+    enemies.append(Creature(random.randint(0,SCREEN_SIZE_W),random.randint(0,SCREEN_SIZE_H), "Image_Files/sprite_0.png", random.randint(10,100)))
 
-updateDisplay()
+player = Player(500,900,"Image_Files/sprite_4.png",100)
+
+while True:
+    for e in enemies:
+        e.move()
+    tick(30)
+    updateDisplay()
+    player.move()
+
+
         
 endWait()
